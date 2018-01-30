ad6de038a89909b29254f6f8a2ed0dfb4aa11dda
kkumshayev <kumshkesh@gmail.com>
2018-01-30 18:04:57 +0000

    update to readme

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 README
 
 I'm refactoring the gilded rose kata!
+
+First I wrote tests to make sure the code was working & then went on to refactor.
+
+The update_quality method was a bit messy to my eye and I decided to rewrite it from scratch instead of attempting to modify it bit by bit.
+I made a diagram of the behaviour of different items and wrote a method focused on each individual item that way things were grouped and less confusing.
--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -42,11 +42,11 @@ class GildedRoseTest(unittest.TestCase):
         self.assertEquals(2, items[0].quality)
 
     def test_sulfuras(self):
-        items = [Item("Sulfuras, Hand of Ragnaros", 1, 1)]
+        items = [Item("Sulfuras, Hand of Ragnaros", 1, 60)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEquals(1, items[0].sell_in)
-        self.assertEquals(1, items[0].quality)
+        self.assertEquals(60, items[0].quality)
 
     def test_conjured(self):
         items = [Item("Conjured Mana Cake", 3, 6)]
@@ -60,6 +60,7 @@ class GildedRoseTest(unittest.TestCase):
             Item("Backstage passes to a TAFKAL80ETC concert", 11, 50),
             Item("Backstage passes to a TAFKAL80ETC concert", 6, 49),
             Item("Backstage passes to a TAFKAL80ETC concert", 1, 48),
+            Item("Aged Brie", -32, 50)
         ]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
@@ -69,6 +70,8 @@ class GildedRoseTest(unittest.TestCase):
         self.assertEquals(50, items[1].quality)
         self.assertEquals(0, items[2].sell_in)
         self.assertEquals(50, items[2].quality)
+        self.assertEquals(-33, items[3].sell_in)
+        self.assertEquals(50, items[3].quality)
 
 
 if __name__ == '__main__':
